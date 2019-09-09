3d77525ead48e8bbe1f3df25ca9389180f6c9958
shallow <1056722757@qq.com>
2019-09-10 00:47:35 +0800

    把本地工程添加git

---
+++ git-test-project/src/git/FirstClass.java
@@ -0,0 +1,15 @@
+package git;
+
+/**
+ *
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project FirstClass");
+        System.out.println("hello git project FirstClass");
+        System.out.println("hello git project FirstClass");
+        System.out.println("hello git project FirstClass");
+    }
+
+}
